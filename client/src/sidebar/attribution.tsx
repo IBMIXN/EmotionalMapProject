ab789c08eec38e500226795c98273c7a381cb0a6
Daniel Stone <me@danstone.uk>
2021-03-16 23:09:51 +0000

    💬 Add GitHub to attribution

--- client/src/sidebar/attribution.tsx
+++ client/src/sidebar/attribution.tsx
@@ -10,6 +10,8 @@ export const Attribution = (): JSX.Element => {
             <Text fontSize="xs">Map data is provided by <a href="https://www.ordnancesurvey.co.uk/business-government/products/boundaryline"><Text as="u">ordnancesurvey.co.uk</Text></a> and <a href="https://www.opendatani.gov.uk/dataset/osni-open-data-50k-boundaries-ni-counties"><Text as="u">opendatani.gov.uk</Text></a>  under the <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"><Text as="u">Open Government Licence v3.0</Text></a></Text>
             <a href="https://developer.twitter.com/"> <Text mt={2} fontSize="xs" as="u">Twitter API</Text></a>
             <a href="https://www.ibm.com/watson/services/tone-analyzer/"> <Text mt={2} fontSize="xs" as="u">IBM Watson Tone Analyzer</Text></a>
+            <br />
+            <a href="https://github.com/IBMIXN/EmotionalMapProject"> <Text mt={2} fontSize="xs">Code available on GitHub</Text></a>
         </React.Fragment>
     );
 };
