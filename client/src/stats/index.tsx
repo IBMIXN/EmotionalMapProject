cfee0a7d128afc1c12c2641f4a83d278295a5255
Daniel Stone <me@danstone.uk>
2021-03-20 17:25:07 +0000

    💬 Add disclaimer to bottom of stats page

--- client/src/stats/index.tsx
+++ client/src/stats/index.tsx
@@ -1,4 +1,4 @@
-import {  Flex,  GridItem, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
+import { Box, Center, Flex, GridItem, SimpleGrid, Text, useBreakpointValue, useColorModeValue } from "@chakra-ui/react";
 import * as React from "react";
 import Leaderboard from "./leaderboard";
 import Pie from "./pie";
@@ -6,13 +6,14 @@ import Trending from "./trending";
 import Map from "../map";
 
 export const Stats = (): JSX.Element => {
+  const mobileMode = useBreakpointValue({ base: true, md: false }) ?? false;
   const bg = useColorModeValue("white", "gray.900");
 
   return (
     <SimpleGrid
       gap={4}
       p={4}
-      columns={[1, null, 2]}
+      columns={mobileMode ? 1 : 2}
       backgroundColor={bg}
     >
       <GridItem >
@@ -26,7 +27,7 @@ export const Stats = (): JSX.Element => {
           p={4}
         >
 
-            <Map onClickEnabled={false} />
+          <Map onClickEnabled={false} />
 
         </Flex>
       </GridItem>
@@ -38,6 +39,16 @@ export const Stats = (): JSX.Element => {
       </GridItem>
       <GridItem >
         <Leaderboard />
+      </GridItem>
+      <GridItem colSpan={mobileMode ? 1 : 2}>
+        <Box m={4} mr={mobileMode ? 16 : 32} ml={mobileMode ? 16 : 32}>
+          <Text textAlign="center" fontSize="sm">
+            That&apos;s all!
+          </Text>
+          <Text textAlign="center" fontSize="xs" mt={2}>
+            The data presented by this application should not be used to make any decisions and further research should be performed to determine conclusions from displayed information.
+          </Text>
+        </Box>
       </GridItem>
     </SimpleGrid>
   );
