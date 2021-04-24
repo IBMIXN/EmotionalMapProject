294fa87c5e3626281914615c91f2dbb6a7223a60
Daniel Stone <me@danstone.uk>
2021-04-24 10:45:45 +0100

    💬 Swap order of attribution and disclaimer

--- client/src/sidebar/attribution.tsx
+++ client/src/sidebar/attribution.tsx
@@ -7,13 +7,13 @@ import React from "react"
 export const Attribution = (): JSX.Element => {
     return (
         <React.Fragment>
+            <Text fontSize="xs">This application is a proof of concept to showcase IBM Watson Tone Analyzer: the data presented should not be used to make any decisions and further research should be performed to determine conclusions from displayed information.</Text>
+            <br/>
             <Text fontSize="xs">Map data is provided by <a href="https://www.ordnancesurvey.co.uk/business-government/products/boundaryline"><Text as="u">ordnancesurvey.co.uk</Text></a> and <a href="https://www.opendatani.gov.uk/dataset/osni-open-data-50k-boundaries-ni-counties"><Text as="u">opendatani.gov.uk</Text></a>  under the <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"><Text as="u">Open Government Licence v3.0</Text></a></Text>
             <a href="https://developer.twitter.com/"> <Text mt={2} fontSize="xs" as="u">Twitter API</Text></a>
             <a href="https://www.ibm.com/watson/services/tone-analyzer/"> <Text mt={2} fontSize="xs" as="u">IBM Watson Tone Analyzer</Text></a>
             <br />
             <a href="https://github.com/IBMIXN/EmotionalMapProject"> <Text mt={2} fontSize="xs">Code available on GitHub</Text></a>
-            <br/>
-            <Text mt={2} fontSize="xs">This application is a proof of concept to showcase IBM Watson Tone Analyzer: the data presented should not be used to make any decisions and further research should be performed to determine conclusions from displayed information.</Text>
         </React.Fragment>
     );
 };
